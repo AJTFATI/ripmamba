a66bdf45cb189de2fd263c4865edae4076ed261c
Alexander J. Taylor <alex@alexanderjtaylor.com>
2026-05-05 16:09:04 -0400

    stroo (reorder socials)

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,5 @@
 import type { Props } from "astro";
 import IconMail from "@/assets/icons/IconMail.svg";
-import IconGitHub from "@/assets/icons/IconGitHub.svg";
 import IconInstagram from "@/assets/icons/IconInstagram.svg";
 import { SITE } from "@/config";
 
@@ -13,22 +12,16 @@ interface Social {
 
 export const SOCIALS: Social[] = [
   {
-    name: "GitHub",
-    href: "https://github.com/ajtfati",
-    linkTitle: `${SITE.title} on GitHub`,
-    icon: IconGitHub,
+    name: "Mail",
+    href: "mailto:contact@alexanderjtaylor.com",
+    linkTitle: `Send an email to ${SITE.title}`,
+    icon: IconMail,
   },
   {
     name: "Instagram",
     href: "https://instagram.com/alexanderjordantaylor",
     linkTitle: `${SITE.title} on Instagram`,
     icon: IconInstagram,
-  },
-  {
-    name: "Mail",
-    href: "mailto:contact@alexanderjtaylor.com",
-    linkTitle: `Send an email to ${SITE.title}`,
-    icon: IconMail,
   },
 ] as const;
 
